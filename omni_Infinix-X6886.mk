a061de96ba5e952879d929335390eed9e71799f3
Fikri Haikal <fikrihaikal2101@gmail.com>
2025-11-05 21:23:25 +0800

    Update Infinix-X6886.mk for device inheritance and branding

--- omni_Infinix-X6886.mk
+++ omni_Infinix-X6886.mk
@@ -5,25 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from Infinix-X6886 device
+# Inherit from Infinix-X678B device
 $(call inherit-product, device/infinix/Infinix-X6886/device.mk)
 
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_X6886
 PRODUCT_DEVICE := Infinix-X6886
-PRODUCT_NAME := omni_Infinix-X6886
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6886
-PRODUCT_MANUFACTURER := infinix
+PRODUCT_MANUFACTURER := INFINIX
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vext_x6886_h8921-user 12 SP1A.210812.016 948331 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6886-OP/Infinix-X6886:12/SP1A.210812.016/250905V2:user/release-keys
